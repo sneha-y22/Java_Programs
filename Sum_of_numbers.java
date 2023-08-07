621b3424eb07c58ece2bcb2822fbf74fbde63281
Sneha Yadav <140615190+sneha-y22@users.noreply.github.com>
2023-08-07 15:09:50 +0530

    Create Sum_of_numbers.java

---
+++ Sum_of_numbers.java
@@ -0,0 +1,13 @@
+package Majorsjava;
+
+//to print the sum of three numbers in java
+
+public class Sum {
+    public static void main(String[] args) {
+
+        // add three numbers
+        int num1=3 , num2= 5, num3 = 19;
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum of three numbers is: "+ sum);
+    }
+}
