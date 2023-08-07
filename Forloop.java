e815573c2a1f79d7df88b1d583eb092a23ed6a71
Sneha Yadav <140615190+sneha-y22@users.noreply.github.com>
2023-08-07 15:17:12 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,17 @@
+package Majorsjava;
+
+// to execute for loop
+
+public class Forloop {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i<n; i++) {
+            for(int j = 0; j<i ; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
