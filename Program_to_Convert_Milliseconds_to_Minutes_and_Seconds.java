720101709a30bc33c71aecb741bf4beb1ef90d0d
Sneha Yadav <140615190+sneha-y22@users.noreply.github.com>
2023-08-12 15:20:15 +0530

    Create Program_to_Convert_Milliseconds_to_Minutes_and_Seconds.java

---
+++ Program_to_Convert_Milliseconds_to_Minutes_and_Seconds.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.TimeUnit;
+
+class Main {
+  public static void main(String[] args) {
+
+    long milliseconds = 1000000;
+
+    long seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds);
+    System.out.println(milliseconds + " Milliseconds = " + seconds + " Seconds");
+
+    long minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds);
+    System.out.println(milliseconds + " Milliseconds = " + minutes + " Minutes");
+  }
+}
