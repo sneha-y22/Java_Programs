3b0fdd77f68e9bde984d6de582c2e97db0e03b2c
Sneha Yadav <140615190+sneha-y22@users.noreply.github.com>
2023-08-12 15:43:28 +0530

    Create Program_to_illustrate_Inheritance.java

---
+++ Program_to_illustrate_Inheritance.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+	int salary = 60000;
+}
+
+class Engineer extends Employee {
+	int benefits = 10000;
+}
+
+class main {
+	public static void main(String args[])
+	{
+		Engineer E1 = new Engineer();
+		System.out.println("Salary : " + E1.salary
+						+ "\nBenefits : " + E1.benefits);
+	}
+}
